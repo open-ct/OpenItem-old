018f4fd7651f39e4b46c3331bdf759d2327d5165
kkoogqw <kkoogqw@hotmial.com>
2021-08-20 19:30:06 +0800

    feat (file basic operation)
    1. finish the file upload / download / search / delete / get APIs.

---
+++ review/routers/commentsRouter_controllers.go
@@ -0,0 +1,172 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["review/controllers:FileController"] = append(beego.GlobalControllerRouter["review/controllers:FileController"],
+		beego.ControllerComments{
+			Method:           "UploadFile",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:FileController"] = append(beego.GlobalControllerRouter["review/controllers:FileController"],
+		beego.ControllerComments{
+			Method:           "DownloadFile",
+			Router:           "/:fid",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:FileController"] = append(beego.GlobalControllerRouter["review/controllers:FileController"],
+		beego.ControllerComments{
+			Method:           "DeleteFile",
+			Router:           "/:fid",
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:FileController"] = append(beego.GlobalControllerRouter["review/controllers:FileController"],
+		beego.ControllerComments{
+			Method:           "GetFileInfo",
+			Router:           "/info/:fid",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:FileController"] = append(beego.GlobalControllerRouter["review/controllers:FileController"],
+		beego.ControllerComments{
+			Method:           "SearchFiles",
+			Router:           "/search",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:ObjectController"] = append(beego.GlobalControllerRouter["review/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:ObjectController"] = append(beego.GlobalControllerRouter["review/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           "/",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:ObjectController"] = append(beego.GlobalControllerRouter["review/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "Get",
+			Router:           "/:objectId",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:ObjectController"] = append(beego.GlobalControllerRouter["review/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "Put",
+			Router:           "/:objectId",
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:ObjectController"] = append(beego.GlobalControllerRouter["review/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "Delete",
+			Router:           "/:objectId",
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UserRegister",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           "/",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UpdateUserInfo",
+			Router:           "/",
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "GetUser",
+			Router:           "/:uid",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UserDelete",
+			Router:           "/:uid",
+			AllowHTTPMethods: []string{"delete"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UserLogin",
+			Router:           "/login",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UserLogout",
+			Router:           "/logout",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["review/controllers:UserController"] = append(beego.GlobalControllerRouter["review/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "UpdateUserPassword",
+			Router:           "/password",
+			AllowHTTPMethods: []string{"put"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
